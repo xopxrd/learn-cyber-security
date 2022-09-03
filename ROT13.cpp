e49c138efeabc155ca983146da84b941e284550b
xopxrd <92743266+xopxrd@users.noreply.github.com>
2022-09-03 18:23:32 +0700

    Update ROT13
    changing the idea of the ROT13

--- ROT13.cpp
+++ ROT13.cpp
@@ -1,96 +1,52 @@
 #include <iostream>
 #include <conio.h>
-#include <fstream>
 
-const int length = 10;
-
-void regist();
-bool login();
+const int character = 13; // the half quantity of the alphabet
+char firstAlphabet[] = {"abcdefghijklm"};
+char lastAlphabet[] = {"nopqrstuvwxyz"};
+char firstCapitalAlphabet[] = {"ABCDEFGHIJKLM"};
+char lastCapitalAlphabet[] = {"NOPQRSTUVWXYZ"};
+char firstNumber[] = {"01234"};
+char lastNumber[] = {"56789"};
 
 int main(){
-	int choice;
-	std::cout << "1. Register\n";
-	std::cout << "2. Login\n";
-	std::cout << "Choice: ";
-	std::cin >> choice;
+	std::string plainText;
+	std::cout << "text encryptor with ROT13\n";
+	std::cout << "insert text: ";
+	getline(std::cin, plainText);
 	
-	switch(choice){
-		case 1:
-			regist();
-			main();
-			break;
-		
-		case 2:
-			{
-			bool status = login();
-			if(!status){
-				std::cout << "invalid username or password!\n";
-			}else{
-				std::cout << "\nSuccesfully logged in!" << std::endl;
-				}
+	// scan the text
+	char cipherText[int(plainText.length())];
+	for(int i = 0; i < int(plainText.length()); i++){
+		if(plainText[i] == ' '){
+			cipherText[i] = plainText[i];
+		}
+		if(plainText[i] != ' '){
+			cipherText[i] = plainText[i];
+		}
+		for(int j = 0; j < character; j++){
+			if(plainText[i] == firstAlphabet[j]){
+				cipherText[i] = lastAlphabet[j];
 			}
-			break;
-		
-		default:
-			std::cout << "invalid input!\n";
+			if(plainText[i] == lastAlphabet[j]){
+				cipherText[i] = firstAlphabet[j];
+			}
+			if(plainText[i] == firstCapitalAlphabet[j]){
+				cipherText[i] = lastCapitalAlphabet[j];
+			}
+			if(plainText[i] == lastCapitalAlphabet[j]){
+				cipherText[i] = firstCapitalAlphabet[j];
+			}
+			if(plainText[i] == lastNumber[j]){
+				cipherText[i] = firstNumber[j];
+			}
+			if(plainText[i] == firstNumber[j]){
+				cipherText[i] = lastNumber[j];
+			}
+		}
 	}
 	
+	std::cout << "chiper text: " << cipherText;
+
 	return 0;
-}
-
-void regist(){
-	std::string name,password;
-	std::cout << "input username: ";
-	std::cin >> name;
-	std::cout << "input password (max 10 characters): ";
-	char hide = getch();
-	for(int i = 0; i < length; i++){
-		if(hide != 13){
-			password.push_back(hide);
-			// hide characters
-			std::cout << "*";
-			hide = getch();
-		}
-	}
-	
-	// ROT 13 encrypt algorithm
-	std::cout << "\nencrypting . . .\n";
-	char encrypt[length];
-	std::ofstream encryptedPass(name + ".txt");
-	encryptedPass << name << std::endl;
-	for(int i = 0; i < int(password.length()); i++){
-		encrypt[i] = int(password[i] + 13);
-		encryptedPass << encrypt[i];
-	}
-	encryptedPass.close();
-	
-	std::cout << std::endl;
-}
-
-bool login(){
-	std::string username, password, uname, pw;
-	std::cout << "Enter username: ";
-	std::cin >> username;
-	std::cout << "Enter password: ";
-	char ch = getch();
-		while(ch != 13){
-			password.push_back(ch);
-			std::cout << "*";
-			ch = getch();
-		}
-	
-	std::ifstream read(username + ".txt");
-	getline(read, uname);
-	getline(read, pw);
-	read.close();
-	
-	char decrypt[length];
-	for(int i = 0; i < int(pw.length()); i++){
-		decrypt[i] = int(pw[i] - 13);
-	}
-	
-	if(username == uname && password == decrypt)
-		return true;
-	else
-		return false;
 }
